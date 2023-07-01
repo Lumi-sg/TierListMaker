007851a042145ec9ccffa1bc3aee8f6e7ba180aa
Lumi <shaicloud@gmail.com>
2023-07-01 16:53:28 -0400

    fixed import tierlist css

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-
-import './App.css'
-import "./reset.css"
-import Header from './Components/Header/Header'
-import Footer from './Components/Footer/Footer'
-import Body from './Components/Body/Body'
+import "./App.css";
+import "./reset.css";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Body from "./Components/Body/Body";
+import ".//Components/Body/styles/tierlist.css";
 
 function App() {
-
-
-  return (<div className="AppContainer">
-    <Header/>
-    <Body/>
-    <Footer/>
-    </div>
-  )
+	return (
+		<div className="AppContainer">
+			<Header />
+			<Body />
+			<Footer />
+		</div>
+	);
 }
 
-export default App
+export default App;
