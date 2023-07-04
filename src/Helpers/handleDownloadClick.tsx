1c0ff9beef81e1922ca3eb6231938296c11f2335
Lumi <shaicloud@gmail.com>
2023-07-04 17:51:28 -0400

    added ignore row options container to html-to-image download

--- src/Helpers/handleDownloadClick.tsx
+++ src/Helpers/handleDownloadClick.tsx
@@ -7,13 +7,24 @@ export const handleDownloadClick = () => {
 		return;
 	}
 
+	// Exclude elements inside the container with the class "pictureIgnore"
+	const elementsToIgnore = tierlistDomElement.querySelectorAll(".pictureIgnore *");
+	elementsToIgnore.forEach((element) => {
+		(element as HTMLElement).style.display = "none"; // Cast to HTMLElement
+	});
+
 	htmlToImage
 		.toJpeg(tierlistDomElement, { quality: 1 })
 		.then(function (dataUrl) {
 			var link = document.createElement("a");
-			link.download = "my-image-name.jpeg";
+			link.download = "mytierlist.jpeg";
 			link.href = dataUrl;
 			link.click();
+
+			// Restore the display of ignored elements after capturing the image
+			elementsToIgnore.forEach((element) => {
+				(element as HTMLElement).style.display = ""; // Cast to HTMLElement
+			});
 		})
 		.catch(function (error) {
 			console.log("Error occurred while capturing the image:", error);
